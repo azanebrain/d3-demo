695b8e7ee2834d5c5edb5215c15243df1adafeb6
AJ Zane <androiddreams@ajzane.com>
2015-01-21 11:21:18 -0800

    Add forcegraph logic
    Replaces 'socialnetwork.js' scrip

--- app/js/forcegraph.js
+++ app/js/forcegraph.js
@@ -1,10 +1,17 @@
+/** Generates a Force Graph
+* Variables:
+* key1 (string) : The first key
+* key2 (string) : The second key
+* label1 (string) : The first label
+* label2 (string) : The second label
+*/
 function render(json) {
   var config = {
     // for d3sparql.graph()
-    // "key1": "taxid1",
-    // "key2": "taxid2",
-    // "label1": "name1",
-    // "label2": "name2",
+    "key1": key1,
+    "key2": key2,
+    "label1": label1,
+    "label2": label2,
     // for d3sparql.forcegraph()
     "radius": function(d) {return 1 + d.label.length},
     "charge": -500,
