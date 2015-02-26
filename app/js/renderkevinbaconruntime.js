445b2dc7f2edd166a9376012b258a98afb5a6d53
AJ Zane <androiddreams@ajzane.com>
2015-02-25 16:19:11 -0800

    add secondary interaction to scatter plot

--- app/js/renderkevinbaconruntime.js
+++ app/js/renderkevinbaconruntime.js
@@ -23,17 +23,49 @@ function render(json) {
     "margin_x": 80,
     "margin_y": 40,
   }
-  // d3sparql.scatterplotwithnodelabel(json, config)
-
   d3sparql.scatterplotwithnodelabel(json, config)
 
-  // Remove the hidden class from the filters interface
-  unhide('filters');
   // Set filter values
   var daterange = document.getElementById("datefilter");
   SVXQuery.data = json.results.bindings;
   daterange.max = d3.max(SVXQuery.data, function(d) { return d.date.value; });
   daterange.min = d3.min(SVXQuery.data, function(d) { return d.date.value; });
   FilterDate.setDateFilter( daterange.defaultValue );
+  // Remove the hidden class from the filters interface
+  unhide('filters');
+
+  // Setup secondary interactivity
+  actorsInNode();
+}
+
+// need an initialize and a set of utility fncns?
+function actorsInNode() {
+  d3.select(graphlocation)
+    .select('svg')
+    .selectAll("g.nodeset")
+    // Perform action when the user clicks an element
+    .on("click", function(d,i) { return visualizeActorsInMovie(d.movieName.value); })
+}
+
+function visualizeActorsInMovie(movieTitle) {
+  // console.debug("Finding actors in movie: " + movieTitle);
+
+  // Get all of the actors in the selected film
+  var sparql =
+      "SELECT DISTINCT ?actorName " +
+      "FROM <movie> " +
+      "WHERE { " +
+        "?movie <http://www.w3.org/2000/01/rdf-schema#label> '" + movieTitle + "'. " +
+        "?movie <http://data.linkedmdb.org/resource/movie/actor> ?actor. " +
+        "?actor <http://data.linkedmdb.org/resource/movie/actor_name> ?actorName.   " +
+      "} " +
+      "ORDER BY asc ( ?actorName ) ";
+  //Query our endpoint and render the results
+  d3sparql.query( SVXQuery.endpoint, sparql, renderActorsInMovie );
+}
+
+function renderActorsInMovie(json) {
+  // Just a basic ugly table
+  // d3sparql.htmltable(json);
 
 }
